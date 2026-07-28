ae7052d14f1452148fd7ddb17d404b56b4500fd5
epi13 <epicureousapc@gmail.com>
2026-07-27 23:17:58 -0800

    Add RAVEL unified execution capsule

---
+++ case-studies/ravel/ravel_unified.c
@@ -0,0 +1,5 @@
+/* Generated RAVEL-U 0.3 execution shards. Do not hand-edit the shards. */
+#include "ravel_unified/00_core.inc"
+#include "ravel_unified/10_route.inc"
+#include "ravel_unified/20_train.inc"
+#include "ravel_unified/30_eval.inc"
